4d84129de17d5645c4d0426c20c52707db869466
Hawkingtale <mhac5527@740s-pl-124.local>
2017-04-25 13:56:12 -0600

    added new methods to add several other shapes

--- src/Drawing/View/ShapePanel.java
+++ src/Drawing/View/ShapePanel.java
@@ -7,21 +7,41 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 
 public class ShapePanel extends JPanel
 {
 
 	private DrawingController baseController;
-	private ArrayList<Rectangle> rectangleList;
+	private ArrayList<Shape> rectangleList;
+	private ArrayList<Shape> triangleList;
+	private ArrayList<Shape> circleList;
+	private ArrayList<Shape> ellipseList;
+	private ArrayList<Shape> polygonList;
+	private ArrayList<ArrayList<Shape>> shapes;
+	
 	
 	
 	public ShapePanel(DrawingController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		rectangleList = new ArrayList<Rectangle>();
+		rectangleList = new ArrayList<Shape>();
+		circleList = new ArrayList<Shape>();
+		ellipseList = new ArrayList<Shape>();
+		triangleList = new ArrayList<Shape>();
+		polygonList = new ArrayList<Shape>();
+		shapes  = new ArrayList<ArrayList<Shape>>();
+		shapes.add(circleList);
+		shapes.add(rectangleList);
+		shapes.add(ellipseList);
+		shapes.add(triangleList);
+		shapes.add(polygonList);
+		
 		
 		setupPanel();
 	}
@@ -55,28 +75,117 @@ public class ShapePanel extends JPanel
 		this.repaint();
 	}
 	
+	public void addCircles()
+	{
+		if(circleList.size() > 500)
+		{
+			circleList.clear();
+		}
+		for(int index = 0; index < 30; index++)
+		{
+			int radius = (int)(Math.random() * 25) +2;
+			int xCorner = (int)(Math.random() * this.getWidth() -15);
+			int yCorner = (int)(Math.random() * this.getHeight() -15);
+			Ellipse2D.Double current = new Ellipse2D.Double(xCorner, yCorner, radius, radius);
+			circleList.add(current);
+		
+		this.repaint();
+		}
+	}
+	
+
+	
+	
+	public void addTriangles()
+	{
+		if(triangleList.size() > 500)
+		{
+			triangleList.clear();
+		}
+		
+		for(int index = 0; index < 30; index++)
+		{
+			int vertexCount = 3;
+			int [] xVertices = new int[vertexCount];
+			int [] yVertices = new int[vertexCount];;
+			for(int vertex = 0; vertex < vertexCount; vertex++)
+			{
+				int xCorner = (int)(Math.random() * this.getHeight());
+				int yCorner = (int)(Math.random() *this.getWidth());
+				xVertices[vertex] = xCorner;
+				yVertices[vertex] = yCorner;
+			}
+		Polygon current = new Polygon(xVertices, yVertices, vertexCount);
+		triangleList.add(current);
+		}
+		this.repaint();
+	}
+	
+	public void addPolygons()
+	{
+		if(polygonList.size() > 500)
+		{
+			polygonList.clear();
+		}
+		
+		for(int index = 0; index < 30; index++)
+		{
+			int vertexCount = (int)(Math.random() * 7) +4;
+			int [] xVertices = new int[vertexCount];
+			int [] yVertices = new int[vertexCount];
+			for(int vertex = 0; vertex < vertexCount; vertex++)
+			{
+				int xCorner = (int)(Math.random() * this.getWidth());
+				int yCorner = (int)(Math.random() * this.getHeight());
+				xVertices[vertex] = xCorner;
+				yVertices[vertex] = yCorner;
+			}
+		}
+	}
+	
+	public void reset()
+	{
+		for(int index = 0; index < shapes.size(); index++)
+		{
+			shapes.get(index).clear();
+		}
+		this.setBackground(getRandomColor());
+		this.repaint();
+	}
 	public void painComponent(Graphics graphics)
 	{
-		this.setBackground(Color.());
-		
 		Graphics2D drawingGraphics = (Graphics2D) graphics;
-		for(Rectangle currentRectangle : rectangleList)
+		
+		for( ArrayList<Shape> currentList : shapes)
 		{
-			drawingGraphics.setColor(getRandomColor());
+			drawShapes(currentList, drawingGraphics);
+		}
+
+	}
+	
+	private void drawShapes(ArrayList<Shape> shapeList, Graphics2D graphics)
+	{
+		
+		for(Shape currentShape : shapeList)
+		{
+			graphics.setColor(getRandomColor());
 			int strokeWidth = (int) (Math.random() * 10) +1;
-			drawingGraphics.setStroke(new BasicStroke(strokeWidth));
+			graphics.setStroke(new BasicStroke(strokeWidth));
 			
-			int randomness = (int) (Math.random() *25);
+			int randomness = (int) (Math.random() *45);
 			
 			if(randomness % 5 == 0 || randomness % 7 == 0)
 			{
-				drawingGraphics.fill(currentRectangle);
+				graphics.fill(currentShape);
+				graphics.setColor(getRandomColor());
+				graphics.draw(currentShape);
 			}
 			
 			else
 			{
-				drawingGraphics.draw(currentRectangle);
+				graphics.draw(currentShape);
 			}
 		}
+		
 	}
 }
