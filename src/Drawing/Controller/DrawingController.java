1201dca96c16860c1122666ad025c4dc172fb879
Hawkingtale <mhac5527@740s-pl-124.local>
2017-04-26 11:51:48 -0600

    added method to drawing Controller to start

--- src/Drawing/Controller/DrawingController.java
+++ src/Drawing/Controller/DrawingController.java
@@ -1,11 +1,15 @@
 package Drawing.Controller;
 
+
+import Drawing.View.DrawingPanel;
 public class DrawingController
 {
 
+	DrawingPanel drawingPanel;
+	
 	public DrawingController()
 	{
-		
+		this.drawingPanel = new DrawingPanel(this);
 	}
 	
 	public void start()
